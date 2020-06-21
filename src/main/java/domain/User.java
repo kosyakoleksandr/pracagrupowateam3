9c56805d04172635ef0ff2eeddcc04c66e4e23ac
aleksandr <kosyakoleksandr87@gmail.com>
2020-06-21 13:39:08 +0300

    Commit

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,9 +1,16 @@
 package domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@AllArgsConstructor
+@Data
 public class User {
      private String id;
      private String name;
      private String surName;
+     private User playlist;
+
+
 }
