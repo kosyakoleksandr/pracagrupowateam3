74b7f860f94c542bb49dae1663810a4d7e169748
Szymon <szymon1106@gmail.com>
2020-06-21 13:52:18 +0200

    Logic update

--- src/main/java/logic/UserService.java
+++ src/main/java/logic/UserService.java
@@ -2,11 +2,17 @@ package logic;
 
 import domain.User;
 
+import java.util.Set;
+
 public interface UserService {
-    void addUser(String name, String id, String surname);
+
+    void addUser(String id, String name, String surname, String playlistId);
+
+    void addPlaylistAutor(String playlistId, String userId);
 
     User findById(String userId);
 
     void delete(String userId);
 
+    Set<User> findPlaylistAutor(String playlistId);
 }
--- src/main/java/logic/UserServiceImpl.java
+++ src/main/java/logic/UserServiceImpl.java
@@ -2,15 +2,26 @@ package logic;
 
 import domain.User;
 import lombok.RequiredArgsConstructor;
+import persistence.UserDAO;
+
+import java.util.Set;
 
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserDAO userDAO;
 
     @Override
-    public void addUser(String id, String name, String surname) {
-        User playlist = serId == null ? null : UserDAO.findById(userId);
-        User user = new User(id, name, surname, playlist);
-        userDAO.save(user);
+    public void addUser(String id, String name, String surname, String playlistId) {
+        User playlist = playlistId == null ? null : userDAO.findById(playlistId);
+        User u = new User(id, name, surname, playlist);
+        userDAO.save(u);
+    }
+
+    public void addPlaylistAutor(String playlistId, String userId) {
+        User user = userDAO.findById(userId);
+        User playlist = userDAO.findById(playlistId);
+        user.setPlaylist(playlist);
+        userDAO.update(user);
     }
 
     @Override
@@ -22,5 +33,9 @@ public class UserServiceImpl implements UserService {
     public void delete(String userId) {
         userDAO.delete(userId);
 
+    }
+    @Override
+    public Set<User> findPlaylistAutor(String playlistId) {
+        return userDAO.findPlaylistAutor(playlistId);
     }
 }
